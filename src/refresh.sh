ce5b143e56e75d8057e71ef6e51d52c66f80bf0c
Marek Pilarczyk <marek.pilarczyk@gmail.com>
2015-04-11 15:10:58 +0200

    Beznadziejny skrypt do budowania i restartowania instancji developerskiej

---
+++ src/refresh.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Skrypt do automatycznego budowania aplikacji w określonych interwałach czasowych - strasznie łopatologicznie, nie paczać!
+
+while [ 1 ]
+do
+    cd ../../plantingjs
+    git pull
+    npm run build
+    cd ../plantingjs-simpleserver/src
+    git pull
+    cp ../../plantingjs/dist static -r
+    killall python
+    python planting.py &
+    sleep 900
+done
